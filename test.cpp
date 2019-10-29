ec5cd67e6c520b88642affbcc0f5e8a957e607e5
gatlee-creator <gatlee@highpoint.edu>
2019-10-29 11:02:03 -0400

    testing functions

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
+#include "orderedArrayListType.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    orderedArrayListType list_1(5);
+    list_1.insertAt(0, 1);
+    list_1.insertAt(1, 2);
+
+    list_1.print(); 
+    cout << list_1.listSize() << endl;
 
     return 0; 
 }
